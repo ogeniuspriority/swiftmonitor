7dbb9ac228f957e1d5224c7b24b5ad976ffa092b
Christian Rutayisire <Christian Rutayisire>
2018-07-16 15:34:31 +0200

    git commit -m "changed the welcome page links"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Swift monitor
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Documentation</a>
+                    <a href="https://web.facebook.com/OGeniusPriority">News</a>
+                    <a href="http://ogeniuspriority.com">About us</a>
+                    <a href="https://github.com/ogeniuspriority/swiftmonitor">GitHub</a>
                 </div>
             </div>
         </div>
