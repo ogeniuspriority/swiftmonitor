fd6bcb4c5140e81d6d4241f5751758c7c60da852
Christian Rutayisire <Christian Rutayisire>
2018-07-16 19:55:19 +0200

    A lot of changes

---
+++ database/migrations/2018_07_16_160158_create_products_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',100);
+            $table->string('age',100);
+            $table->string('gender',10);
+            $table->string('brand',50);
+            $table->string('category',50);
+            $table->integer('pieces');
+            $table->integer('initialpriceperunit');
+            $table->integer('minimumpriceperunit');
+            $table->integer('maximumpriceperunit');
+            $table->string('color');
+            $table->text('description');
+            $table->text('careinstructions');
+            $table->string('material');
+           
+            $table->string('code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
